bb8e199c362343ccc28290cf6b5755d72aadf243
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-16 20:46:59 +0500

    finisheeed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
 import { Toaster } from "react-hot-toast";
@@ -9,11 +9,13 @@ import MainContext from "./context/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <MainContext>
-        <App />
-        <Toaster position="top-center" reverseOrder={false} />
-      </MainContext>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <MainContext>
+          <App />
+          <Toaster position="top-center" reverseOrder={false} />
+        </MainContext>
+      </Provider>
+    </Router>
   </BrowserRouter>
 );
