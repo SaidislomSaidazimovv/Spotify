bdadbf43b56e7c58196191152d034372e004054c
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-16 20:56:32 +0500

    somee problems

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
 import { Toaster } from "react-hot-toast";
@@ -11,9 +11,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <MainContext>
-        <Router>
           <App />
-        </Router>
         <Toaster position="top-center" reverseOrder={false} />
       </MainContext>
     </Provider>
